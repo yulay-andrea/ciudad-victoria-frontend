207675a8a3134bb85a212446790c54e9a31345dc
alejandro <JAGUIR4@bancodebogota.com.co>
2021-01-11 20:30:24 -0500

    modificacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PrincipalComponent } from './principal/principal.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'index', component: PrincipalComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
