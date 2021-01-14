9a722f4129261d1b8ae98905e966367ce3e94ca1
alejandro <JAGUIR4@bancodebogota.com.co>
2021-01-13 21:31:19 -0500

    modificacion en index

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { PrincipalComponent } from './principal/principal.component';
 
 const routes: Routes = [
-  { path: 'index', component: PrincipalComponent },
+  { path: '',   redirectTo: '/index', pathMatch: 'full' },
+  { path: 'index', component: PrincipalComponent}
 ];
 
 @NgModule({
